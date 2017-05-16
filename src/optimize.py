59c1beabb4abaaa72815c8190f06158dac30be45
Alexander Gunin <guninalexander@gmail.com>
2017-05-15 20:42:54 -0700

    distributed run

--- src/optimize.py
+++ src/optimize.py
@@ -10,25 +10,25 @@ CONTENT_LAYER = 'relu4_2'
 DEVICES = 'CUDA_VISIBLE_DEVICES'
 
 # np arr, np arr
-def optimize(content_targets, style_target, content_weight, style_weight,
+def optimize(cluster,task_index,num_gpus,content_targets, style_target, content_weight, style_weight,
              tv_weight, vgg_path, epochs=2, print_iterations=1000,
-             batch_size=4, save_path='saver/fns.ckpt', slow=False,
+             batch_size=4, save_path='saver',
              learning_rate=1e-3, debug=False):
-    if slow:
-        batch_size = 1
+    server = tf.train.Server(
+            cluster, job_name="worker", task_index=task_index)
     mod = len(content_targets) % batch_size
     if mod > 0:
         print("Train set has been trimmed slightly..")
-        content_targets = content_targets[:-mod] 
+        content_targets = content_targets[:-mod]
 
     style_features = {}
 
     batch_shape = (batch_size,256,256,3)
     style_shape = (1,) + style_target.shape
     print(style_shape)
-
+    is_chief = (task_index == 0)
     # precompute style features
-    with tf.Graph().as_default(), tf.device('/cpu:0'), tf.Session() as sess:
+    with tf.Graph().as_default(), tf.device('/cpu:0'), tf.Session():
         style_image = tf.placeholder(tf.float32, shape=style_shape, name='style_image')
         style_image_pre = vgg.preprocess(style_image)
         net = vgg.net(vgg_path, style_image_pre)
@@ -38,8 +38,16 @@ def optimize(content_targets, style_target, content_weight, style_weight,
             features = np.reshape(features, (-1, features.shape[3]))
             gram = np.matmul(features.T, features) / features.size
             style_features[layer] = gram
-
-    with tf.Graph().as_default(), tf.Session() as sess:
+    if num_gpus>0:
+        worker_device = "/job:worker/task:%d/gpu:0" % (task_index)
+    else:
+        worker_device = "/job:worker/task:%d/cpu:0" % (task_index)
+    with tf.device(
+            tf.train.replica_device_setter(
+                worker_device=worker_device,
+                ps_device="/job:ps/cpu:0",
+                cluster=cluster)):
+        global_step = tf.Variable(0, name="global_step", trainable=False)
         X_content = tf.placeholder(tf.float32, shape=batch_shape, name="X_content")
         X_pre = vgg.preprocess(X_content)
 
@@ -48,14 +56,8 @@ def optimize(content_targets, style_target, content_weight, style_weight,
         content_net = vgg.net(vgg_path, X_pre)
         content_features[CONTENT_LAYER] = content_net[CONTENT_LAYER]
 
-        if slow:
-            preds = tf.Variable(
-                tf.random_normal(X_content.get_shape()) * 0.256
-            )
-            preds_pre = preds
-        else:
-            preds = transform.net(X_content/255.0)
-            preds_pre = vgg.preprocess(preds)
+        preds = transform.net(X_content/255.0)
+        preds_pre = vgg.preprocess(preds)
 
         net = vgg.net(vgg_path, preds_pre)
 
@@ -88,54 +90,67 @@ def optimize(content_targets, style_target, content_weight, style_weight,
         loss = content_loss + style_loss + tv_loss
 
         # overall loss
-        train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-        sess.run(tf.global_variables_initializer())
+        train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step)
+
+        init_op = tf.global_variables_initializer()
+
+        sv = tf.train.Supervisor(
+                is_chief=is_chief,
+                logdir=save_path,
+                init_op=init_op,
+                recovery_wait_secs=1,
+                global_step=global_step)
+
+        sess_config = tf.ConfigProto(
+            allow_soft_placement=True,
+            log_device_placement=False,
+            device_filters=["/job:ps", "/job:worker/task:%d" % task_index])
+
+        sess = sv.prepare_or_wait_for_session(server.target, config=sess_config)
+
         import random
         uid = random.randint(1, 100)
         print("UID: %s" % uid)
-        for epoch in range(epochs):
-            num_examples = len(content_targets)
-            iterations = 0
-            while iterations * batch_size < num_examples:
-                start_time = time.time()
-                curr = iterations * batch_size
-                step = curr + batch_size
-                X_batch = np.zeros(batch_shape, dtype=np.float32)
-                for j, img_p in enumerate(content_targets[curr:step]):
-                   X_batch[j] = get_img(img_p, (256,256,3)).astype(np.float32)
+        local_step = 0
+        num_examples = len(content_targets)
+        iterations = 0
+        num_samples = num_examples / batch_size
+        num_global =  num_samples * epochs
+        print("Number of iterations %d" % num_global)
+        while True:
+            start_time = time.time()
+            curr = iterations * batch_size
+            step = curr + batch_size
+            X_batch = np.zeros(batch_shape, dtype=np.float32)
+            for j, img_p in enumerate(content_targets[curr:step]):
+               X_batch[j] = get_img(img_p, (256,256,3)).astype(np.float32)
 
-                iterations += 1
-                assert X_batch.shape[0] == batch_size
+            iterations += 1
+            if iterations == num_samples:
+                iterations = 0
+            assert X_batch.shape[0] == batch_size
 
-                feed_dict = {
+            feed_dict = {
+               X_content:X_batch
+            }
+            _, step = sess.run([train_step, global_step], feed_dict=feed_dict)
+            local_step += 1
+            print("Worker %d: training step %d done (global step: %d)" %
+                (task_index, local_step, step))
+
+            end_time = time.time()
+            delta_time = end_time - start_time
+            if debug:
+                print("UID: %s, batch time: %s" % (uid, delta_time))
+            if step >= num_global:
+                to_get = [style_loss, content_loss, tv_loss, loss, preds]
+                test_feed_dict = {
                    X_content:X_batch
                 }
-
-                train_step.run(feed_dict=feed_dict)
-                end_time = time.time()
-                delta_time = end_time - start_time
-                if debug:
-                    print("UID: %s, batch time: %s" % (uid, delta_time))
-                is_print_iter = int(iterations) % print_iterations == 0
-                if slow:
-                    is_print_iter = epoch % print_iterations == 0
-                is_last = epoch == epochs - 1 and iterations * batch_size >= num_examples
-                should_print = is_print_iter or is_last
-                if should_print:
-                    to_get = [style_loss, content_loss, tv_loss, loss, preds]
-                    test_feed_dict = {
-                       X_content:X_batch
-                    }
-
-                    tup = sess.run(to_get, feed_dict = test_feed_dict)
-                    _style_loss,_content_loss,_tv_loss,_loss,_preds = tup
-                    losses = (_style_loss, _content_loss, _tv_loss, _loss)
-                    if slow:
-                       _preds = vgg.unprocess(_preds)
-                    else:
-                       saver = tf.train.Saver()
-                       res = saver.save(sess, save_path)
-                    yield(_preds, losses, iterations, epoch)
+                tup = sess.run(to_get, feed_dict = test_feed_dict)
+                _style_loss,_content_loss,_tv_loss,_loss,_preds = tup
+                losses = (_style_loss, _content_loss, _tv_loss, _loss)
+                return (_preds, losses, iterations, epochs)
 
 def _tensor_size(tensor):
     from operator import mul
--- style.py
+++ style.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import sys, os, pdb
 sys.path.insert(0, 'src')
-import numpy as np, scipy.misc 
+import numpy as np, scipy.misc
 from optimize import optimize
 from argparse import ArgumentParser
 from utils import save_img, get_img, exists, list_files
+import tensorflow as tf
 import evaluate
 
 CONTENT_WEIGHT = 7.5e0
@@ -21,9 +22,15 @@ BATCH_SIZE = 4
 DEVICE = '/gpu:0'
 FRAC_GPU = 1
 
+
+PS_HOSTS = 'localhost:2222'
+WORKER_HOSTS = 'localhost:2222'
+JOB_NAME = 'worker'
+TASK_INDEX = 0
+
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument('--checkpoint-dir', type=str,
+    parser.add_argument('--train_dir', type=str,
                         dest='checkpoint_dir', help='dir to save checkpoint in',
                         metavar='CHECKPOINT_DIR', required=True)
 
@@ -42,10 +49,6 @@ def build_parser():
     parser.add_argument('--test-dir', type=str,
                         dest='test_dir', help='test image save dir',
                         metavar='TEST_DIR', default=False)
-
-    parser.add_argument('--slow', dest='slow', action='store_true',
-                        help='gatys\' approach (for debugging, not supported)',
-                        default=False)
 
     parser.add_argument('--epochs', type=int,
                         dest='epochs', help='num epochs',
@@ -69,7 +72,7 @@ def build_parser():
                         dest='content_weight',
                         help='content weight (default %(default)s)',
                         metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
-    
+
     parser.add_argument('--style-weight', type=float,
                         dest='style_weight',
                         help='style weight (default %(default)s)',
@@ -79,11 +82,33 @@ def build_parser():
                         dest='tv_weight',
                         help='total variation regularization weight (default %(default)s)',
                         metavar='TV_WEIGHT', default=TV_WEIGHT)
-    
+
     parser.add_argument('--learning-rate', type=float,
                         dest='learning_rate',
                         help='learning rate (default %(default)s)',
                         metavar='LEARNING_RATE', default=LEARNING_RATE)
+    parser.add_argument('--ps_hosts', type=str,
+                        dest='ps_hosts',
+                        help='comma-separated list of hostname:port pairs',
+                        metavar='PS_HOSTS', default='localhost:2222')
+    parser.add_argument('--worker_hosts', type=str,
+                        dest='worker_hosts',
+                        help='comma-separated list of hostname:port pairs',
+                        metavar='WORKER_HOSTS', default='localhost:3333')
+    parser.add_argument('--job_name', type=str,
+                        dest='job_name',
+                        help='job name: worker or ps',
+                        metavar='JOB_NAME', default='worker')
+    parser.add_argument('--task_index', type=int,
+                        dest='task_index',
+                        help="Worker task index, should be >= 0. task_index=0 is "
+                        "the master worker task the performs the variable "
+                        "initialization ",
+                        metavar='TASK_INDEX', default=0)
+    parser.add_argument('--num_gpus', type=int,
+                        dest='num_gpus',
+                        help='Total GPU Number',
+                        metavar='NUM_GPU', default=0)
 
     return parser
 
@@ -93,7 +118,8 @@ def check_opts(opts):
     exists(opts.train_path, "train path not found!")
     if opts.test or opts.test_dir:
         exists(opts.test, "test img not found!")
-        exists(opts.test_dir, "test directory not found!")
+        if not os.path.exists(opts.test_dir):
+            os.makedirs(opts.test_dir)
     exists(opts.vgg_path, "vgg network data not found!")
     assert opts.epochs > 0
     assert opts.batch_size > 0
@@ -103,39 +129,51 @@ def check_opts(opts):
     assert opts.style_weight >= 0
     assert opts.tv_weight >= 0
     assert opts.learning_rate >= 0
+    assert opts.task_index >=0
 
 def _get_files(img_dir):
     files = list_files(img_dir)
     return [os.path.join(img_dir,x) for x in files]
 
-    
+
 def main():
     parser = build_parser()
     options = parser.parse_args()
+
+    ps_spec = options.ps_hosts.split(",")
+    worker_spec = options.worker_hosts.split(",")
+
+    # Get the number of workers.
+    num_workers = len(worker_spec)
+
+    cluster = tf.train.ClusterSpec({
+        "ps": ps_spec,
+        "worker": worker_spec})
+    if options.job_name == "ps":
+        print("Start parameter server %d" % (options.task_index))
+        server = tf.train.Server(
+            cluster, job_name=options.job_name, task_index=options.task_index)
+        server.join()
+        return
+
     check_opts(options)
 
     style_target = get_img(options.style)
-    if not options.slow:
-        content_targets = _get_files(options.train_path)
-    elif options.test:
-        content_targets = [options.test]
+
+    content_targets = _get_files(options.train_path)
 
     kwargs = {
-        "slow":options.slow,
         "epochs":options.epochs,
         "print_iterations":options.checkpoint_iterations,
         "batch_size":options.batch_size,
-        "save_path":os.path.join(options.checkpoint_dir,'fns.ckpt'),
+        "save_path":options.checkpoint_dir,
         "learning_rate":options.learning_rate
     }
 
-    if options.slow:
-        if options.epochs < 10:
-            kwargs['epochs'] = 1000
-        if options.learning_rate < 1:
-            kwargs['learning_rate'] = 1e1
-
     args = [
+        cluster,
+        options.task_index,
+        options.num_gpus,
         content_targets,
         style_target,
         options.content_weight,
@@ -144,21 +182,18 @@ def main():
         options.vgg_path
     ]
 
-    for preds, losses, i, epoch in optimize(*args, **kwargs):
-        style_loss, content_loss, tv_loss, loss = losses
+    preds, losses, i, epoch = optimize(*args, **kwargs)
+    style_loss, content_loss, tv_loss, loss = losses
 
-        print('Epoch %d, Iteration: %d, Loss: %s' % (epoch, i, loss))
-        to_print = (style_loss, content_loss, tv_loss)
-        print('style: %s, content:%s, tv: %s' % to_print)
-        if options.test:
-            assert options.test_dir != False
-            preds_path = '%s/%s_%s.png' % (options.test_dir,epoch,i)
-            if not options.slow:
-                ckpt_dir = os.path.dirname(options.checkpoint_dir)
-                evaluate.ffwd_to_img(options.test,preds_path,
-                                     options.checkpoint_dir)
-            else:
-                save_img(preds_path, img)
+    print('Epoch %d, Iteration: %d, Loss: %s' % (epoch, i, loss))
+    to_print = (style_loss, content_loss, tv_loss)
+    print('style: %s, content:%s, tv: %s' % to_print)
+    if options.test and (options.task_index == 0):
+        assert options.test_dir != False
+        preds_path = '%s/%s_%s.png' % (options.test_dir,epoch,i)
+        ckpt_dir = os.path.dirname(options.checkpoint_dir)
+        evaluate.ffwd_to_img(options.test,preds_path,
+                                 options.checkpoint_dir)
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
