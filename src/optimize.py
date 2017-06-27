51746896db313e360e5b94d99118e83462573f01
Alexander Gunin <guninalexander@gmail.com>
2017-06-27 02:54:57 -0700

    export

--- src/optimize.py
+++ src/optimize.py
@@ -154,6 +154,7 @@ def optimize(cluster,task_index,num_gpus,limit,content_targets, style_target, co
             print("Training ends @ %f" % time_end)
             training_time = time_end - time_begin
             print("Training elapsed time: %f s" % training_time)
+            sv.request_stop()
         return (_preds, losses, iterations, epochs)
 
 def _tensor_size(tensor):
